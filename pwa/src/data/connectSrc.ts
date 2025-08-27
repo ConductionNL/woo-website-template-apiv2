031e57462fac96031af37d7e06b014a01b501c3d
Mwest2020 <mwesterweel@hotmail.com>
2025-08-27 11:05:30 +0200

    roosendaal

--- pwa/src/data/connectSrc.ts
+++ pwa/src/data/connectSrc.ts
@@ -98,7 +98,7 @@ https://rijssen-holten.openwoo.app
 https://rijssen-holten.accept.openwoo.app
 https://roosendaal.openwoo.app
 https://roosendaal.accept.openwoo.app
-https://roosendaal.prod.commonground.nu
+https://roosendaal.commonground.nu
 https://roosendaal.accept.commonground.nu
 https://rotterdam.openwoo.app
 https://rotterdam.accept.openwoo.app
