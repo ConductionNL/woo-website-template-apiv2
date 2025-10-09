a8ab3a419688f15ee9e348e109c4a734cfd89193
Alex Rekachynskyi <phone.070718@gmail.com>
2025-10-09 13:10:48 +0200

    Logo is not minimised anymore

--- pwa/gatsby-node.js
+++ pwa/gatsby-node.js
@@ -7,103 +7,21 @@ exports.onCreateBabelConfig = ({ actions }) => {
   });
 };
 
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig, plugins }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  const config = getConfig();
+
+  if (config.optimization) {
+    config.optimization.minimize = false;
+  }
+
   if (stage === "develop" || stage === "build-javascript") {
-    const config = getConfig();
-    const miniCssExtractPlugin = config.plugins.find(
+    const miniCssExtractPlugin = config.plugins?.find(
       (plugin) => plugin.constructor.name === "MiniCssExtractPlugin",
     );
     if (miniCssExtractPlugin) {
       miniCssExtractPlugin.options.ignoreOrder = true;
     }
-
-    const options = {
-      minimizerOptions: {
-        preset: [
-          `default`,
-          {
-            svgo: {
-              plugins: [
-                {
-                  name: "preset-default",
-                  params: {
-                    overrides: {
-                      // Disable plugins that can break SVGs
-                      removeViewBox: false, // Keep viewBox for proper scaling
-                      removeDimensions: false, // Keep width/height attributes
-                      cleanupIds: false, // Prevent ID reference issues
-                      removeTitle: false, // Keep titles for accessibility
-                      removeDesc: false, // Keep descriptions for accessibility
-                    },
-                  },
-                },
-                // Additional safe optimization plugins
-                "removeUselessDefs",
-                "cleanupAttrs",
-                "cleanupEnableBackground",
-                "cleanupListOfValues",
-                "cleanupNumericValues",
-                "collapseGroups",
-                "convertColors",
-                "convertPathData",
-                "convertStyleToAttrs",
-                "convertTransform",
-                "inlineStyles",
-                "mergePaths",
-                "minifyStyles",
-                "moveElemsAttrsToGroup",
-                "moveGroupAttrsToElems",
-                "removeComments",
-                "removeDoctype",
-                "removeEditorsNSData",
-                "removeEmptyAttrs",
-                "removeEmptyContainers",
-                "removeEmptyText",
-                "removeHiddenElems",
-                "removeMetadata",
-                "removeNonInheritableGroupAttrs",
-                "removeOffCanvasPaths",
-                "removeRasterImages",
-                "removeScriptElement",
-                "removeUnknownsAndDefaults",
-                "removeUnusedNS",
-                "removeUselessStrokeAndFill",
-                "removeXMLProcInst",
-                "reusePaths",
-                "sortAttrs",
-              ],
-            },
-          },
-        ],
-      },
-    };
-
-    actions.replaceWebpackConfig(config);
   }
 
-  // Configure CSS optimization to preserve SVGs
-  if (stage === "build-css") {
-    const config = getConfig();
-
-    // Find and configure the CSS minimizer to disable SVGO
-    const minimizer = config.optimization.minimizer.find(
-      (plugin) => plugin.constructor.name === "CssMinimizerPlugin",
-    );
-
-    if (minimizer) {
-      minimizer.options = {
-        ...minimizer.options,
-        minimizerOptions: {
-          preset: [
-            "default",
-            {
-              svgo: false, // Disable SVGO in CSS to preserve SVG data URIs
-            },
-          ],
-        },
-      };
-    }
-
-    actions.replaceWebpackConfig(config);
-  }
+  actions.replaceWebpackConfig(config);
 };
